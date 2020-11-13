8df977e37d62e0fc852938ece42d263ab4d3ef5c
Fabian Hueber <fabi_boy@hotmail.de>
2020-11-13 16:07:24 +0100

    jo

---
+++ DrunkenboldeServer/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DrunkenboldeServer.Startup))]
+
+namespace DrunkenboldeServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
